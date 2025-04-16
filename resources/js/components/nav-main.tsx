9266062d4298be933c61fda6550c9e94f4d789a4
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-04-16 18:05:32 +0400

    feat: add Tabs component and integrate into ViewContract page for improved UI navigation

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -10,11 +10,14 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
             <SidebarGroupLabel>Platform</SidebarGroupLabel>
             <SidebarMenu>
                 {items.map((item) => {
-                    const hrefPath = new URL(item.href, window.location.origin).pathname;
+                    const hrefPath = new URL(item.href, window.location.origin).pathname.split('/').slice(1);
+                    const pageUrl = page.url.split('/').slice(1);
+                    // console.log('hrefPath', hrefPath[0]);
+                    // console.log('pageUrl', pageUrl[0]);
                     return (
                         <SidebarMenuItem key={item.title}>
-                            <SidebarMenuButton asChild isActive={hrefPath === page.url} tooltip={{ children: item.title }}>
-                                <Link href={hrefPath} prefetch>
+                            <SidebarMenuButton asChild isActive={hrefPath[0] === pageUrl[0]} tooltip={{ children: item.title }}>
+                                <Link href={item.href} prefetch>
                                     {item.icon && <item.icon />}
                                     <span>{item.title}</span>
                                 </Link>
