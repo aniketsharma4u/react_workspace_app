658292079336ab423fea7ea9cac38ec63c229805
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-06 15:54:51 +0400

    feat(vite.config): add build configuration for output directory and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,9 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+    },
 });
