c1ca0ecca887075979a2d02081daec99ec1b6988
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-06 15:57:16 +0400

    fix(vite.config): set base path for application

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/react_workspace_app/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
