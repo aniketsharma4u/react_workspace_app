9b7f00a51aa687df9115f60d678620c2af37eff9
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-08 13:16:07 +0400

    fix(AppLogoIcon): update base path for logo image source

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -3,6 +3,6 @@ interface AppLogoIconProps {
 }
 
 export default function AppLogoIcon({ className = 'w-[170px]' }: AppLogoIconProps) {
-    const basePath = import.meta.env.APP_URL || ''; // Use REACT_APP_ prefix
+    const basePath = import.meta.env.ASSET_URL || ''; // Use REACT_APP_ prefix
     return <img src={`${basePath}/assets/img/logo.png`} className={className} alt="App Logo" />;
 }
