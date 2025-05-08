499516f11c6bf31e6d2ab8fbfa4d47fb462adaa9
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-08 12:21:32 +0400

    fix(vite.config): update output directory path for build

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: 'public/build',
+        outDir: 'react_workspace_app/public/build',
         emptyOutDir: true,
         manifest: true,
     },
