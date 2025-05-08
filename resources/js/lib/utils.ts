f23a41390eb3852ade954a41e5a26ae7f131d60a
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-08 15:41:53 +0400

    UP 3

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,6 +4,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
-export function publicPath(path: string): string {
-    return `${window.location.origin}/public/${path.replace(/^\/+/, '')}`;
+export function publicPath(): string {
+    return `${window.location.origin}/public`;
 }
