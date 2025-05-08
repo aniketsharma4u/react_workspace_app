cf14dfa4213b8244088cb31f73b83f0c75d1dd92
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-08 14:37:48 +0400

    fix(AppLogoIcon): update base path to use props.ziggy.url as fallback

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,8 +1,13 @@
+import { type SharedData } from '@/types';
+import { usePage } from '@inertiajs/react';
+
 interface AppLogoIconProps {
     className?: string;
 }
 
 export default function AppLogoIcon({ className = 'w-[170px]' }: AppLogoIconProps) {
-    const basePath = import.meta.env.VITE_ASSET_URL || ''; // Use REACT_APP_ prefix
+    const { props } = usePage<SharedData>();
+    // console.log('AppLogoIcon data:', props.ziggy.url);
+    const basePath = import.meta.env.VITE_ASSET_URL || props.ziggy.url; // Use REACT_APP_ prefix
     return <img src={`${basePath}/assets/img/logo.png`} className={className} alt="App Logo" />;
 }
