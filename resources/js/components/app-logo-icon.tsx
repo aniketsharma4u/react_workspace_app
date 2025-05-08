590f3a83303a662a62df092f0445cd11a750c18b
Aniket Sharma <aniket.sharma@moreideas.ae>
2025-05-08 15:32:17 +0400

    fix(AppLogoIcon): refactor logo image source to use publicPath utility

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,13 +1,9 @@
-import { type SharedData } from '@/types';
-import { usePage } from '@inertiajs/react';
+import { publicPath } from '@/lib/utils';
 
 interface AppLogoIconProps {
     className?: string;
 }
 
 export default function AppLogoIcon({ className = 'w-[170px]' }: AppLogoIconProps) {
-    const { props } = usePage<SharedData>();
-    // console.log('AppLogoIcon data:', props.ziggy.url);
-    const basePath = import.meta.env.VITE_ASSET_URL || props.ziggy.url; // Use REACT_APP_ prefix
-    return <img src={`${basePath}/assets/img/logo.png`} className={className} alt="App Logo" />;
+    return <img src={publicPath('/assets/img/logo.png')} className={className} alt="App Logo" />;
 }
--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+export function publicPath(path: string): string {
+    return `${window.location.origin}/${path.replace(/^\/+/, '')}`;
+}
